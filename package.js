edf43f525c1f93c8170c7638833d56701e8b8480
Nathan Reid <nathan-code@nathantreid.com>
2015-11-04 22:57:52 -0600

    updated to latest version of mss compiler

--- package.js
+++ package.js
@@ -14,7 +14,7 @@ Package.describe({
 Package.registerBuildPlugin({
 	name: 'css-modules-css-build-plugin',
 	use: [
-		'nathantreid:css-modules-mss-compiler@0.0.1'
+		'nathantreid:css-modules-mss-compiler@0.0.2'
 	],
 	sources: [
 		'plugin.js',
